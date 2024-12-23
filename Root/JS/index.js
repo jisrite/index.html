7bf4686d6ff06d780458ad4279502d63fb6548d4
John Solitario <4mirite@gmail.com>
2024-12-22 23:25:36 -0800

    First Commit

---
+++ Root/JS/index.js
@@ -0,0 +1,7 @@
+
+// create an animation for when review is submitted.
+// thinking a little fade in , "Thank you!" animation would suffice.
+// will have a function that fetches the submit button to console
+// all three labels will be fetched via ".value"
+// at the end with the fade in animation, can add placeholder to 
+// say, "Thank you! placehold (${})". to change text, make sure to use ".textContent"
